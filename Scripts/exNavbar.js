459190083c17d77d9a6d6ef7b1ea8ee6c495921e
skshm2000 <107468465+skshm2000@users.noreply.github.com>
2022-08-23 19:37:05 +0530

    Day-1 Footer&Navbar Exports

---
+++ Scripts/exNavbar.js
@@ -0,0 +1,16 @@
+function navbar() {
+    return `<img src="https://www.sendinblue.com/wp-content/themes/sendinblue2019/assets/images/common/logo-color.svg" alt="send in blue logo">
+    <div>
+        <p id="solutions">Solutions</p>
+        <p id="pricing">Pricing</p>
+        <p id="features">Features</p>
+        <p id="resources">Resources</p>
+        <p id="blog">Blog</p>
+    </div>
+    <div>
+        <p id="login">Log in</p>
+        <button id="signup">Sign up free</button>
+    </div>`
+}
+
+export default navbar
